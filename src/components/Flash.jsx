e71c312fbd36d42d7467940ce4794be1d126adc9
Normar Macawile <nmacawile@gmail.com>
2023-11-01 04:24:21 +0000

    Fix flash text appearing behind card image

--- src/components/Flash.jsx
+++ src/components/Flash.jsx
@@ -4,7 +4,7 @@ import "../styles/Flash.css";
 export function Flash({ flash }) {
   const currentScore = useSelector((state) => state.score.value).current;
   const textClasses =
-    "flash-text transition-all duration-600 ease-in-out select-none text-8xl font-bold absolute -translate-y-2/4 -translate-x-2/4 left-2/4 top-2/4";
+    "flash-text z-[11] transition-all duration-600 ease-in-out select-none text-8xl font-bold absolute -translate-y-2/4 -translate-x-2/4 left-2/4 top-2/4";
 
   return (
     <h1
