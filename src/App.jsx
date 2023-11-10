488ad1d42dc2da2456896c936fc1096c97710d5a
Normar Macawile <nmacawile@gmail.com>
2023-11-10 07:57:48 +0000

    Fix overflow issue at certain resolutions

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Modal from "./components/Modal.jsx";
 
 export function App() {
   return (
-    <div className="dark:bg-neutral-800 h-full">
+    <div className="dark:bg-neutral-800 h-full overflow-auto">
       <Header />
       <Modal />
       <main className="max-w-6xl w-full m-auto dark:bg-neutral-800">
